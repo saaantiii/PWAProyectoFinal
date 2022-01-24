f9cd29c89524744bc7bd888a33d4e6929e2caad0
Santi Sarchetti <santiagosarchetti@gmail.com>
2022-01-24 19:17:47 -0300

    3rt

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ connection.connect((error) => {
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 
 app.use(session({
